741c161feb23169080febfd0ca29c1c7a3066fe8
unikmavraj <unikmavraj@icloud.com>
2026-05-12 20:50:24 +0200

    done

---
+++ module 22/js/main.js
@@ -0,0 +1,16 @@
+console.log("Goodbye");
+
+var x = 10;
+
+var _value = "Digital School";
+
+//var 123 = 55; //wrong way
+
+//var *value = 56; //wrong
+
+//
+
+
+//1.   Numbers:5, 6.5, 7, etc.
+
+//2.    String: "Hello GeeksforGeeks"
