1ddac907655e66c9efc01710a3797a109ec2462c
unikmavraj <unikmavraj@icloud.com>
2026-05-21 20:44:02 +0200

    u

---
+++ module 26/main.js
@@ -0,0 +1,55 @@
+/*var a = 50 + 50;
+console.log(a);
+
+var b=20;
+
+var c = 30;
+
+var d = b + c;
+
+console.log(d);
+
+var s = 32;
+var k = 44;
+
+var h = s * k;*/
+
+/*
+var a = 99;
+a++;
+console.log(a);
+a--;
+console.log(a);
+*/
+
+/*
+var x = 15;
+x-=5;
+console.log(x)
+*/
+
+
+// var x = 5;
+
+// var y = 5;
+
+// console.log(x == y);
+
+
+/*var school = 'our school';
+console.log('hello from $(school)')
+
+
+var var3 = 9;
+var var4 = 10;
+
+console.log(!(var3 == var4));*/
+
+
+var a = 5;
+var b = 6;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
---
+++ module 27/main.js
@@ -0,0 +1,55 @@
+// function showmessage(){
+//    alert('This is my first function')
+// }
+// showmessage()
+
+// function sum(numri1, numri2){
+//     return numri1+numri2
+// }
+
+// function calculate(n1, n2){
+//     if("+"){
+//         return n1+n2
+//     }
+
+//      if("-"){
+//         return n1-n2
+//     }
+
+//      if("*"){
+//         return n1*n2
+//     }
+
+//      if("/"){
+//         return n1/n2
+//     }
+
+// }
+
+
+function digitalschool(){
+    var shkolla = 'digitalschool'
+    alert(shkolla)
+
+}
+
+digitalschool()
+
+
+function area_of_triangle(base, height){
+    return base * height * 0.5
+}
+
+console.log('area of triangle is:' + area_of_triangle(5,7))
+
+
+var person = 1{
+    name:"person1"
+    age:"67"
+    blood_type:"O+"
+
+    person:function
+
+}
+
+alert(person)
